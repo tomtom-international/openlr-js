277022ded94caa66be0f53abcc50f50b0c65b29d
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2017-11-21 11:00:07 +0100

    Build multiple target (es5, es6) and rollup for browser

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
+const plugins = [
+    resolve({
+        preferBuiltins: false
+    }),
+    commonjs()
+];
+
+if (process.env.ROLLUP_UGLIFY === 'true') {
+    plugins.push(uglify());
+}
+
+export default {
+    input: 'lib-es5/index.js',
+    output: {
+        format: 'umd'
+    },
+    name: 'OpenLR',
+    plugins
+};
