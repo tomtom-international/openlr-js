997d59fd40ec810c338bc0c0daaeeb15c0d582cd
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2020-01-28 14:57:58 +0100

    Updated copyright notice

--- src/data/raw-location-reference/RawPointAlongLineLocationReference.ts
+++ src/data/raw-location-reference/RawPointAlongLineLocationReference.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 TomTom International B.V
+ * Copyright 2020 TomTom International B.V
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
