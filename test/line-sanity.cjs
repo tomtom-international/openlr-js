34eec3b4e065c7fbb2f77fa413de9e5dd950a308
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2025-06-23 14:25:25 +0200

    comment out test case that is outside the limits of openlr spec

--- test/line-sanity.cjs
+++ test/line-sanity.cjs
@@ -59,49 +59,51 @@ if (openLrString !== encodedOpenLrString) {
   throw new Error("Expected OpenLR string to be equal: " + openLrString + " and " + encodedOpenLrString)
 }
 
-// Check 3 specific points and make sure the intermediate results are OK.
-let locations = [
-  {lng: 4.7538936137926395, lat: 52.374883889902236},
-  {lng: 4.7563336137926395, lat: 52.373583889902235},
-  {lng: 5.1, lat: 52.0}
-]
+// NOTE: the following cases are commented out because they are not within the limits of the OpenLR specification.
 
-// Encode
-let rawLineLocationReference = RawLineLocationReference.fromLineValues(
-  "binary",
-  locations.map((location, i) => {
-    return LocationReferencePoint.fromValues(
-      i,
-      7,
-      0,
-      location.lng,
-      location.lat,
-      0,
-      i < locations.length - 1 ? 100 : 0,
-      7,
-      i === locations.length - 1
-    )
-  }),
-  Offsets.fromValues(0, 0)
-)
-serializedRawLocationReference = Serializer.serialize(rawLineLocationReference)
-deserializedRawLocationReference = Serializer.deserialize(serializedRawLocationReference)
-encodedLocationReference = binaryEncoder.encodeDataFromRLR(deserializedRawLocationReference)
-encodedOpenLrBinary = encodedLocationReference.getLocationReferenceData()
-encodedOpenLrString = encodedOpenLrBinary.toString("base64")
-const almost = (x, y) => Math.abs(x - y) < 0.0001
-// Decode
-openLrBinary = Buffer.from(encodedOpenLrString, "base64")
-locationReference = LocationReference.fromIdAndBuffer("binary", openLrBinary)
-rawLocationReference = binaryDecoder.decodeData(locationReference)
-locations.map((location, i) => {
-    let locationReferencePoint = rawLocationReference.getLocationReferencePoints()[i]
-    if (!almostEqual(locationReferencePoint.getLongitudeDeg(), location.lng) || !almostEqual(locationReferencePoint.getLatitudeDeg(), location.lat)) {
-      throw new Error("Expected location to be equal: " + location.lng + "," + location.lat + " !== " + locationReferencePoint.getLongitudeDeg() + "," + locationReferencePoint.getLatitudeDeg())
-    }
-  }
-)
+// // Check 3 specific points and make sure the intermediate results are OK.
+// let locations = [
+//   {lng: 4.7538936137926395, lat: 52.374883889902236},
+//   {lng: 4.7563336137926395, lat: 52.373583889902235},
+//   {lng: 5.1, lat: 52.0}
+// ]
 
-if (openLrString !== encodedOpenLrString) {
-  throw new Error("Expected OpenLR string to be equal: " + openLrString + " and " + encodedOpenLrString)
-}
+// // Encode
+// let rawLineLocationReference = RawLineLocationReference.fromLineValues(
+//   "binary",
+//   locations.map((location, i) => {
+//     return LocationReferencePoint.fromValues(
+//       i,
+//       7,
+//       0,
+//       location.lng,
+//       location.lat,
+//       0,
+//       i < locations.length - 1 ? 100 : 0,
+//       7,
+//       i === locations.length - 1
+//     )
+//   }),
+//   Offsets.fromValues(0, 0)
+// )
+// serializedRawLocationReference = Serializer.serialize(rawLineLocationReference)
+// deserializedRawLocationReference = Serializer.deserialize(serializedRawLocationReference)
+// encodedLocationReference = binaryEncoder.encodeDataFromRLR(deserializedRawLocationReference)
+// encodedOpenLrBinary = encodedLocationReference.getLocationReferenceData()
+// encodedOpenLrString = encodedOpenLrBinary.toString("base64")
+// const almost = (x, y) => Math.abs(x - y) < 0.0001
+// // Decode
+// openLrBinary = Buffer.from(encodedOpenLrString, "base64")
+// locationReference = LocationReference.fromIdAndBuffer("binary", openLrBinary)
+// rawLocationReference = binaryDecoder.decodeData(locationReference)
+// locations.map((location, i) => {
+//     let locationReferencePoint = rawLocationReference.getLocationReferencePoints()[i]
+//     if (!almostEqual(locationReferencePoint.getLongitudeDeg(), location.lng) || !almostEqual(locationReferencePoint.getLatitudeDeg(), location.lat)) {
+//       throw new Error("Expected location to be equal: " + location.lng + "," + location.lat + " !== " + locationReferencePoint.getLongitudeDeg() + "," + locationReferencePoint.getLatitudeDeg())
+//     }
+//   }
+// )
+
+// if (openLrString !== encodedOpenLrString) {
+//   throw new Error("Expected OpenLR string to be equal: " + openLrString + " and " + encodedOpenLrString)
+// }
